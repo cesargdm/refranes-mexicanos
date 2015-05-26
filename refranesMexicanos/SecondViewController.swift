bbe0d45948db98642c3c2e2be2986e48b43f7abb
César Guadarrama <cesargdm@MacBook-Air-de-Cesar-3.local>
2015-05-26 09:33:03 -0500

    Initial Commit

---
+++ refranesMexicanos/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  refranesMexicanos
+//
+//  Created by César Guadarrama on 5/26/15.
+//  Copyright (c) 2015 César Guadarrama. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
